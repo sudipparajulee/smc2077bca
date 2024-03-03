aeff974a12e2984d8edcec0b45cff6abdc9d7883
Sudip Parajuli <sudip.parajulee@gmail.com>
2024-03-03 09:15:22 +0545

    payment integration completed

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -5,9 +5,10 @@
             @foreach ($carts as $cart)
                 <div class="bg-gray-100 p-4 rounded-lg shadow">
                         <div class="flex justify-between items-center">
-                            <p>Product: {{$cart->product_id}}</p>
+                            <img src="{{asset('images/products/'.$cart->product->photopath)}}" class="h-16" alt="">
+                            <p>Product: {{$cart->product->name}}</p>
                             <p>Qty: {{$cart->quantity}}</p>
-                            <p>Price: {{$cart->price}}</p>
+                            <p>Price: {{$cart->product->price}}</p>
                             <a href="{{route('checkout',$cart->id)}}" class="bg-blue-600 text-white px-4 py-2 rounded-lg shadow ">Order Now</a>
                         </div>
                 </div>
