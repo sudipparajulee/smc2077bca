996a89ab2d9a37764d6cb5abb787dc9cedd36968
Sudip Parajuli <sudip.parajulee@gmail.com>
2024-08-02 11:03:42 +0545

    related products

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -23,6 +23,10 @@
         @endif
         <nav class="flex sticky top-0 justify-between shadow px-20 py-4 items-center bg-orange-500">
             <img src="https://icms-image.slatic.net/images/ims-web/e6ac6883-1158-4663-bda4-df5a1aa066e5.png" class="w-24" alt="">
+            <form action="" class="flex-1 w-full px-10 mt-5 flex items-center gap-4">
+                <input type="text" class="w-full block rounded-lg" placeholder="Search any Product">
+                <button class="bg-blue-600 text-white px-3 py-2 rounded-lg">Search</button>
+            </form>
             <div class="text-white font-bold">
                 <a class="mx-2" href="/">Home</a>
                 @php
--- resources/views/viewproduct.blade.php
+++ resources/views/viewproduct.blade.php
@@ -25,4 +25,17 @@
         <h2 class="font-bold text-3xl">Product Description</h2>
         <p class="text-gray-700">{{$product->description}}</p>
     </div>
+
+
+    <h2 class="mt-10 text-4xl font-bold px-24">Related Products</h2>
+    <div class="grid my-10 grid-cols-4 gap-10 px-24">
+        @foreach($relatedproducts as $product)
+        <a href="{{route('viewproduct',$product->id)}}" class="border p-4 rounded shadow hover:shadow-md hover:-translate-y-1 duration-500">
+            <img src="{{asset('images/products/'.$product->photopath)}}" class="w-full h-64 object-cover" alt="">
+            <h2 class="text-xl font-bold my-2">{{$product->name}}</h2>
+            <p class="text-gray-700 line-clamp-2">{{$product->description}}</p>
+            <p class="text-gray-700 font-bold mt-2">Rs {{$product->price}}</p>
+        </a>
+        @endforeach
+    </div>
 @endsection
